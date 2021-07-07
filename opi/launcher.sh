0b82ba98fb9fa587fb591f199df646adbd3ca6b9
Giles Knap <giles.knap@diamond.ac.uk>
2021-07-07 21:12:05 +0100

    add priviledge option for podman edm image

--- opi/launcher.sh
+++ opi/launcher.sh
@@ -9,12 +9,14 @@ then
     # try podman if we dont see docker installed
     shopt -s expand_aliases
     alias docker='podman'
+    opts= "--privilege "
 fi
 
 image=gcr.io/diamond-pubreg/controls/python3/s03_utils/epics/edm:latest
 environ="-e DISPLAY=$DISPLAY -e EDMDATAFILES=/screens"
 volumes="-v ${thisdir}/${ioc}:/screens -v /tmp:/tmp"
+opts=${opts}"-ti"
 
 set -x
 xhost +local:docker
-docker run ${environ} ${volumes} ${@} -ti ${image} edm -x -noedit ${start}
+docker run ${environ} ${volumes} ${@} ${opts} ${image} edm -x -noedit ${start}
