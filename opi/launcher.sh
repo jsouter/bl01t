875742d20abcf3163bf896d8c1af70e33c4d3186
Giles Knap <gilesknap@gmail.com>
2023-03-25 16:35:46 +0000

    use epics-containers edm image

--- opi/launcher.sh
+++ opi/launcher.sh
@@ -12,10 +12,11 @@ then
     opts= "--privilege "
 fi
 
-image=gcr.io/diamond-pubreg/controls/python3/s03_utils/epics/edm:latest
+image=ghcr.io/epics-containers/edm:latest
 environ="-e DISPLAY -e EPICS_CA_ADDR_LIST -e EPICS_CA_AUTO_ADDR_LIST -e EDMDATAFILES=/screens"
 volumes="-v ${thisdir}/${ioc}:/screens -v /tmp:/tmp"
 opts=${opts}"-ti --net=host"
 
 set -x
 docker run --rm -d ${environ} ${volumes} ${@} ${opts} ${image} edm -x -noedit ${start}
+
