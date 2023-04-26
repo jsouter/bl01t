cf84b80d89e99f4aac2a2469caa7270f745ebb2c
Giles Knap <giles.knap@diamond.ac.uk>
2023-04-26 07:54:48 +0000

    fix edm launcher

--- opi/launcher.sh
+++ opi/launcher.sh
@@ -5,19 +5,11 @@ start=${2}
 shift 2
 thisdir=$(realpath $(dirname ${BASH_SOURCE[0]}))
 
-if [ -z $(which docker 2> /dev/null) ]
-then
-    # try podman if we dont see docker installed
-    shopt -s expand_aliases
-    alias docker='podman'
-    opts= "--privilege "
-fi
-
 image=ghcr.io/epics-containers/edm:latest
 environ="-e DISPLAY -e EPICS_CA_ADDR_LIST -e EPICS_CA_AUTO_ADDR_LIST -e EPICS_CA_SERVER_PORT -e EDMDATAFILES=/screens"
 volumes="-v ${thisdir}/${ioc}:/screens -v /tmp:/tmp"
 opts=${opts}"-ti --privileged --net=host"
 
 set -x
-docker run --rm -d ${environ} ${volumes} ${@} ${opts} ${image} edm -x -noedit ${start}
+podman run --rm -d ${environ} ${volumes} ${@} ${opts} ${image} edm -x -noedit ${start}
 
